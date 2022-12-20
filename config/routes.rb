4017bdc1c659b99c4b6b4b2ffe3f31f16564c2a2
Jerry Owusu <jerryowusu6133@gmail.com>
2022-12-20 22:02:48 +0000

    Update sign-up form views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end 
   
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_211711) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
